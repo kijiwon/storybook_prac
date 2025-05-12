de9a8275e32a5ddc3ad062a8cf6b3f94303040ab
kijiwon <kiji1313@gmail.com>
2025-05-12 23:26:08 +0900

    msw 파일 추가

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,11 +1,12 @@
+
 /* A simple redux store/actions/reducer implementation.
  * A true app would be more complex and separated into different files.
  */
 import {
   configureStore,
   createSlice,
-  createAsyncThunk,
-} from "@reduxjs/toolkit";
+ createAsyncThunk,
+} from '@reduxjs/toolkit';
 
 /*
  * The initial state of our store when the app loads.
@@ -14,7 +15,7 @@ import {
 
 const TaskBoxData = {
   tasks: [],
-  status: "idle",
+  status: 'idle',
   error: null,
 };
 
@@ -23,18 +24,18 @@ const TaskBoxData = {
  * You can read more about Redux Toolkit's thunks in the docs:
  * https://redux-toolkit.js.org/api/createAsyncThunk
  */
-export const fetchTasks = createAsyncThunk("todos/fetchTodos", async () => {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/todos?userId=1"
-  );
-  const data = await response.json();
-  const result = data.map((task) => ({
-    id: `${task.id}`,
-    title: task.title,
-    state: task.completed ? "TASK_ARCHIVED" : "TASK_INBOX",
-  }));
-  return result;
-});
+ export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
+   const response = await fetch(
+     'https://jsonplaceholder.typicode.com/todos?userId=1'
+   );
+   const data = await response.json();
+   const result = data.map((task) => ({
+     id: `${task.id}`,
+     title: task.title,
+     state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+   }));
+   return result;
+ });
 
 /*
  * The store is created here.
@@ -42,7 +43,7 @@ export const fetchTasks = createAsyncThunk("todos/fetchTodos", async () => {
  * https://redux-toolkit.js.org/api/createSlice
  */
 const TasksSlice = createSlice({
-  name: "taskbox",
+  name: 'taskbox',
   initialState: TaskBoxData,
   reducers: {
     updateTaskState: (state, action) => {
@@ -59,23 +60,23 @@ const TasksSlice = createSlice({
    */
   extraReducers(builder) {
     builder
-      .addCase(fetchTasks.pending, (state) => {
-        state.status = "loading";
-        state.error = null;
-        state.tasks = [];
-      })
-      .addCase(fetchTasks.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.error = null;
-        // Add any fetched tasks to the array
-        state.tasks = action.payload;
-      })
-      .addCase(fetchTasks.rejected, (state) => {
-        state.status = "failed";
-        state.error = "Something went wrong";
-        state.tasks = [];
-      });
-  },
+    .addCase(fetchTasks.pending, (state) => {
+      state.status = 'loading';
+      state.error = null;
+      state.tasks = [];
+    })
+    .addCase(fetchTasks.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.error = null;
+      // Add any fetched tasks to the array
+      state.tasks = action.payload;
+     })
+    .addCase(fetchTasks.rejected, (state) => {
+      state.status = 'failed';
+      state.error = "Something went wrong";
+      state.tasks = [];
+    });
+ },
 });
 
 // The actions contained in the slice are exported for usage in our components
